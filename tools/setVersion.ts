b676cc49af5058a737ad7f2923142b8dfea27dd1
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-03-16 12:57:34 +0900

    feat: バージョン用のスクリプトを追加

---
+++ tools/setVersion.ts
@@ -0,0 +1,15 @@
+import fs from "node:fs/promises";
+
+if (process.argv.length !== 3) {
+  console.error("Usage: node setVersion.js <version>");
+  process.exit(1);
+}
+
+const version = process.argv[2];
+
+const appVue = await fs.readFile("landing/src/App.vue", "utf-8");
+
+await fs.writeFile(
+  "landing/src/App.vue",
+  appVue.replace(/const version = ".*";/, `const version = "${version}";`),
+);
